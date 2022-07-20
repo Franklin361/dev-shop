f7b7f07519c1b86b07a2ea5f471da56715f01cf4
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-07-20 18:23:16 -0500

    [FEAT] add layout to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,14 @@
+import { ShopLayout } from "../components"
+
+// TODO: custom 404 with an image
 const Custom404 = () => {
     return (
-        <div>Custom</div>
+        <ShopLayout>
+            <section className="p-5 flex justify-center flex-col items-center gap-5">
+                <span className="text-9xl font-extrabold text-accent">404</span>
+                <span className="text-5xl font-bold text-accent">Page Not Found</span>
+            </section>
+        </ShopLayout>
     )
 }
 
