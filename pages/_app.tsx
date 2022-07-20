a5d87cff96e00865b74eec72cbd3eda941f3fcc3
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-07-20 00:36:32 -0500

    [STYLE] add meta tag url page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,12 +19,12 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         <meta property="og:title" content="Dev Shop - Online Store For Developers" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="" />
+        <meta property="og:url" content="https://dev-shop.vercel.app" />
         <meta property="og:image" content="" />
         <meta property="og:description" content="This is an online store that offers products for programmers from all over the world, find everything you need right here! 🤯" />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content="" />
+        <meta name="twitter:url" content="https://dev-shop.vercel.app" />
         <meta name="twitter:description" content="This is an online store that offers products for programmers from all over the world, find everything you need right here! 🤯" />
         <meta name="twitter:title" content="Dev Shop - Online Store For Developers" />
         <meta name="twitter:site" content="@Frankomtz361" />
