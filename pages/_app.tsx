dc8c9c7a9ff0b3a65d90b564231174e0a01dd326
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-07-20 15:11:17 -0500

    [FEAT] add layout to home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,19 +17,19 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="description" content="This is an online store that offers products for programmers from all over the world, find everything you need right here! 🤯" />
         <meta name="author" content="Franklin Martinez" />
 
-        <meta property="og:title" content="Dev Shop - Online Store For Developers" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://dev-shop.vercel.app" />
+        <meta property="og:title" content="Dev Shop - Online Store For Developers" />
         <meta property="og:image" content="" />
+        <meta property="og:url" content="https://dev-shop.vercel.app" />
         <meta property="og:description" content="This is an online store that offers products for programmers from all over the world, find everything you need right here! 🤯" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:url" content="https://dev-shop.vercel.app" />
         <meta name="twitter:description" content="This is an online store that offers products for programmers from all over the world, find everything you need right here! 🤯" />
+        <meta name="twitter:image" content="" />
         <meta name="twitter:title" content="Dev Shop - Online Store For Developers" />
         <meta name="twitter:site" content="@Frankomtz361" />
         <meta name="twitter:creator" content="@Frankomtz361" />
-        <meta name="twitter:image" content="" />
 
         <link rel="shortcut icon" href="https://res.cloudinary.com/dnxchppfm/image/upload/c_scale,w_991/v1658294651/Shopping_perspective_matte_ytgkal.webp" type="image/x-icon" />
         <title>Dev Shop</title>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
 import type { NextPage } from 'next'
+import { Overlay, ShopLayout } from '../components'
+import { ILayoutShop } from '../interfaces'
+
+const headProps: ILayoutShop = {
+  imgUrl: '',
+  pageDesc: 'Find the best products for developers!',
+  title: 'Dev-Shop | Home'
+}
+
+// TODO: DOWNSIZE  IMAGE HEADER
 
 const Home: NextPage = () => {
   return (
-    <div className='font-bold text-primary'>Dev - Shop</div>
+    <ShopLayout {...headProps}>
+      <header className=' bg-[url("https://i.pinimg.com/736x/e7/b1/e2/e7b1e231979b4603ff0489f372998e74.jpg")] bg-center bg-no-repeat bg-cover min-h-[50vh] flex justify-center items-center flex-col gap-5 relative rounded overflow-hidden'>
+
+        <h1 className='font-normal text-8xl z-20'>
+          <span className='font-extrabold'>Dev</span>
+          Shop
+        </h1>
+        <h2 className='z-20 text-2xl'> Found all the products you need! </h2>
+        <Overlay />
+
+      </header>
+
+      <section className='min-h-screen'>
+
+      </section>
+    </ShopLayout>
   )
 }
 
