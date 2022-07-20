48386cf662b83b58227f86b7e7981be1fa6dccb4
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-07-20 00:33:13 -0500

    [STYLE] add daysi ui and metatags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,43 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
+import '../styles/globals.css'
+
+// TODO: update meta tags
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <link rel="shortcut icon" href="https://res.cloudinary.com/dnxchppfm/image/upload/c_scale,w_991/v1658294651/Shopping_perspective_matte_ytgkal.webp" type="image/x-icon" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta name="description" content="This is an online store that offers products for programmers from all over the world, find everything you need right here! 🤯" />
+        <meta name="author" content="Franklin Martinez" />
+
+        <meta property="og:title" content="Dev Shop - Online Store For Developers" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="" />
+        <meta property="og:image" content="" />
+        <meta property="og:description" content="This is an online store that offers products for programmers from all over the world, find everything you need right here! 🤯" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content="" />
+        <meta name="twitter:description" content="This is an online store that offers products for programmers from all over the world, find everything you need right here! 🤯" />
+        <meta name="twitter:title" content="Dev Shop - Online Store For Developers" />
+        <meta name="twitter:site" content="@Frankomtz361" />
+        <meta name="twitter:creator" content="@Frankomtz361" />
+        <meta name="twitter:image" content="" />
+
+        <title>Dev Shop</title>
+        <link rel="shortcut icon" href="https://res.cloudinary.com/dnxchppfm/image/upload/c_scale,w_991/v1658294651/Shopping_perspective_matte_ytgkal.webp" type="image/x-icon" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
