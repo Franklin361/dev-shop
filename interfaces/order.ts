4052e1dedd329ea309647c23136f4f101914313d
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-08-07 23:31:48 -0500

    [FEAT] create order model

---
+++ interfaces/order.ts
@@ -0,0 +1,29 @@
+import { IUser } from "./user"
+import { InfoAddress } from './cart';
+import { ISize } from "./product";
+
+export interface IOrder {
+    _id?: string
+    user?: IUser | string
+    orderItems: IOrderItem[]
+    shippingAddress: InfoAddress
+    paymentResult?: string
+
+    numberOfItems: number
+    subtotal: number
+    tax: number
+    total: number
+
+    isPaid: boolean
+    paidAt?: string
+}
+
+export interface IOrderItem {
+    _id: string
+    title: string
+    size: ISize
+    quantity: number
+    slug: string
+    image: string
+    price: number
+}
