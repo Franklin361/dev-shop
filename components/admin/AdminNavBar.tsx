9c98367f5f0b71c39d24bf3ebce016db1384e16e
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-08-09 12:38:23 -0500

    [FEAT] add admin nav bar

---
+++ components/admin/AdminNavBar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import { Icon } from "../"
+
+export const AdminNavBar = () => {
+
+    return (
+        <nav className="bg-black/90 shadow-2xl shadow-black/70 p-5 sticky top-0 left-0 z-30 flex justify-center items-center md:pr-10 pr-5">
+            <div className="flex-1 flex">
+                <Link href='/'>
+                    <h4 className="cursor-pointer select-none"><b>Dev</b> | Shop</h4>
+                </Link>
+            </div>
+
+            <div className="flex items-center justify-end gap-5 flex-1">
+
+                <label htmlFor="my-drawer" className="drawer-button p-2  rounded cursor-pointer flex gap-2 hover:bg-white/20 md:hidden">
+                    <span className="select-none">Menu</span>
+                    <Icon className="text-2xl" name="menu" />
+                </label>
+            </div>
+        </nav>
+    )
+}
+
