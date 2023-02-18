4f9388e8876a5e0840f2e090d5a8885974898d94
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2023-02-18 12:08:32 -0600

    style: add less padding

--- components/layout/AdminLayout.tsx
+++ components/layout/AdminLayout.tsx
@@ -1,25 +1,27 @@
 import Head from 'next/head';
 import { Drawer, AdminNavBar } from '../';
-import { ILayoutShop } from "../../interfaces"
+import { ILayoutShop } from '../../interfaces';
 
 interface Props extends ILayoutShop {
-    children: JSX.Element | JSX.Element[]
+  children: JSX.Element | JSX.Element[];
 }
 
-export const AdminLayout = ({ children, title = 'Dev Shop | Admin' }: Props) => {
-    return (
-        <div>
+export const AdminLayout = ({
+  children,
+  title = 'Dev Shop | Admin',
+}: Props) => {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Drawer>
+        <AdminNavBar />
 
-            <Head>
-                <title>{title}</title>
-            </Head>
-            <Drawer>
-                <AdminNavBar />
-
-                <main className="mx-auto w-full max-w-screen-xl px-4 mb-16">
-                    {children}
-                </main>
-            </Drawer>
-        </div>
-    )
-}
+        <main className='mx-auto w-full max-w-screen-xl px-3 mb-16'>
+          {children}
+        </main>
+      </Drawer>
+    </div>
+  );
+};
