aa8d56df1ce94b267d093dc45f8bc2e6c1c38af4
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2023-02-18 12:54:24 -0600

    changes in session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,51 +1,51 @@
-import type { AppProps } from 'next/app'
-import { SWRConfig } from 'swr'
-import { SessionProvider } from "next-auth/react"
-import { HeaderGeneric } from '../components'
+import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
+import { SessionProvider } from 'next-auth/react';
+import { HeaderGeneric } from '../components';
 
-import { Element } from '../interfaces'
+import { Element } from '../interfaces';
 
-import { AuthProvider } from '../context'
+import { AuthProvider } from '../context';
 
-import 'react-slideshow-image/dist/styles.css'
-import '../styles/globals.css'
-import { PayPalScriptProvider } from '@paypal/react-paypal-js'
+import 'react-slideshow-image/dist/styles.css';
+import '../styles/globals.css';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
-
-
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, session }: any) {
   return (
     <>
       <HeaderGeneric />
-      <AppState>
+      <AppState session={session}>
         <Component {...pageProps} />
       </AppState>
-
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
 
 interface Props {
-  children: Element,
-  // session: any
+  children: Element;
+  session: any;
 }
 
-export const AppState = ({ children }: Props) => {
+export const AppState = ({ children, session }: Props) => {
   return (
-    <SessionProvider>
-      <PayPalScriptProvider options={{ "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID as string }}>
+    <SessionProvider session={session}>
+      <PayPalScriptProvider
+        options={{
+          'client-id': process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID as string,
+        }}
+      >
         <SWRConfig
           value={{
-            fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+            fetcher: (resource, init) =>
+              fetch(resource, init).then((res) => res.json()),
           }}
         >
-          <AuthProvider>
-            {children}
-          </AuthProvider>
+          <AuthProvider>{children}</AuthProvider>
         </SWRConfig>
       </PayPalScriptProvider>
     </SessionProvider>
-  )
-}
+  );
+};
